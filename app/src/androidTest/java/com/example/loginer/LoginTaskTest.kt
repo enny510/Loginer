ebc4214a1d27653d846f32c132a276fb0e8c4403
Kate Antoniuk <KAntaniuk@gomel.iba.by>
2019-04-01 09:08:17 +0300

    add comments

--- app/src/androidTest/java/com/example/loginer/LoginTaskTest.kt
+++ app/src/androidTest/java/com/example/loginer/LoginTaskTest.kt
@@ -19,51 +19,67 @@ class LoginTaskTest: BaseTest() {
 
     @Test
     fun validLoginPasswordTest() {
+        // when set valid login and then valid password
         LoginScreen(device).setLogin(VALID_LOGIN).setPassword(VALID_PASSWORD)
+        // then current screen is success login screen
         device.wait(Until.findObject(SuccessLoginScreen.screenMarker), BaseTest.LAUNCH_TIMEOUT)
     }
 
     @Test
     fun validPasswordLoginTest() {
+        // when set valid password and then valid login
         LoginScreen(device).setPassword(VALID_PASSWORD).setLogin(VALID_LOGIN)
+        // then current screen is success login screen
         device.wait(Until.findObject(SuccessLoginScreen.screenMarker), BaseTest.LAUNCH_TIMEOUT)
     }
 
     @Test
     fun loginWithInvalidDataTest() {
+        // when set invalid login and then invalid password
         LoginScreen(device).setLogin("t").setPassword("t")
-        device.wait(Until.findObject(SuccessLoginScreen.screenMarker), BaseTest.LAUNCH_TIMEOUT)
+        // then current screen still login screen
+        device.wait(Until.findObject(LoginScreen.screenMarker), BaseTest.LAUNCH_TIMEOUT)
     }
 
     @Test
     fun loginWithoutPasswordTest() {
+        //when set valid login and stay password field empty
         LoginScreen(device).setLogin(VALID_LOGIN)
+        // then current screen still login screen
         device.wait(Until.findObject(LoginScreen.screenMarker), BaseTest.LAUNCH_TIMEOUT)
     }
 
     @Test
     fun loginWithoutLoginTest() {
+        //when set valid password and stay password field empty
         LoginScreen(device).setPassword(VALID_PASSWORD)
+        // then current screen still login screen
         device.wait(Until.findObject(LoginScreen.screenMarker), BaseTest.LAUNCH_TIMEOUT)
     }
 
     @Test
     fun loginWithWrongLoginTest() {
+        //when set invalid login and valid password
         LoginScreen(device).setLogin("t").setPassword(VALID_PASSWORD)
+        // then current screen still login screen
         device.wait(Until.findObject(LoginScreen.screenMarker), BaseTest.LAUNCH_TIMEOUT)
     }
 
     @Test
     fun loginWithWrongPasswordTest() {
+        //when set valid login and invalid password
         LoginScreen(device).setLogin(VALID_LOGIN).setPassword("t")
+        // then current screen still login screen
         device.wait(Until.findObject(LoginScreen.screenMarker), BaseTest.LAUNCH_TIMEOUT)
     }
 
     @Test
     fun loginAsValidUserAndBackTest() {
+        // when success log in and press back button on success login screen
         val successLoginScreen = LoginScreen(device).authorizeAsUser(User(VALID_LOGIN, VALID_PASSWORD))
         device.wait(Until.findObject(SuccessLoginScreen.screenMarker), BaseTest.LAUNCH_TIMEOUT)
         successLoginScreen.clickBack()
+        // then current screen is login screen
         device.wait(Until.findObject(LoginScreen.screenMarker), BaseTest.LAUNCH_TIMEOUT)
     }
 }
